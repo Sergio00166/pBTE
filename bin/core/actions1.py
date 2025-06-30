c7427337db780822dc7950ac9f46a49db0c6472c
Sergio Bañón <35770464+Sergio00166@users.noreply.github.com>
2025-06-30 15:14:19 +0200

    Revert copy/cut/paste code

--- bin/core/actions1.py
+++ bin/core/actions1.py
@@ -9,14 +9,14 @@ def paste(copy_buffer, arr, line, offset, banoff, cursor, select, rows, status_s
         if len(select)==0:
             pos = line+offset-banoff
             text = arr[pos]
-            p1,p2 = text[:cursor],text[cursor:]
+            p1,p2 = text[:cursor-1],text[cursor-1:]
             if isinstance(copy_buffer,list):
                 arr[pos] = p1+copy_buffer[0]
                 for i, new_line in enumerate(copy_buffer[1:], start=1):
                     arr.insert(pos+i,new_line)
                 arr[pos+len(copy_buffer)-1] += p2
                 line,offset = calc_displacement(copy_buffer[1:],line,banoff,offset,rows)
-                cursor = len(copy_buffer[-1])
+                cursor = len(copy_buffer[-1])+1
             else:
                 arr[pos] = p1+copy_buffer+p2
                 cursor += len(copy_buffer)
@@ -27,10 +27,10 @@ def paste(copy_buffer, arr, line, offset, banoff, cursor, select, rows, status_s
                 for i, new_line in enumerate(copy_buffer):
                     arr.insert(start+i,new_line)
                 line,offset = calc_displacement(copy_buffer,line,banoff,offset,rows,1)
-                cursor = len(copy_buffer[-1])
+                cursor = len(copy_buffer[-1])+1
             else:
                 arr.insert(start,copy_buffer)
-                cursor = len(copy_buffer)
+                cursor = len(copy_buffer)+1
         status_st = False
     return cursor, arr, copy_buffer, line, offset, select, status_st
 
@@ -39,12 +39,12 @@ def cut(select, arr, line, offset, banoff, copy_buffer, cursor):
     pos = line+offset-banoff
     text = arr[pos]
     if select:
-        start = max(sum(select[0]), 0)
+        start = max(sum(select[0])-1, 0)
         copy_buffer = arr[start:sum(select[1])]
         if start>0: copy_buffer = copy_buffer[1:]
         select,arr,line,offset = del_sel(select,arr,banoff)
     else:
-        if cursor==0:
+        if cursor==1:
             if pos==len(arr)-1:
                  if not text == "":
                      copy_buffer = text
@@ -53,12 +53,12 @@ def cut(select, arr, line, offset, banoff, copy_buffer, cursor):
                 copy_buffer = text
                 arr.pop(pos)
 
-        elif cursor==len(text):
+        elif cursor-1==len(text):
             if pos<len(arr)-1:
                 copy_buffer = arr.pop(pos+1)
         else:
-            arr[pos] = text[:cursor]
-            copy_buffer = text[cursor:]
+            arr[pos] = text[:cursor-1]
+            copy_buffer = text[cursor-1:]
 
     if isinstance(copy_buffer,list) and len(copy_buffer)==1:
         copy_buffer = copy_buffer[0]
@@ -67,16 +67,16 @@ def cut(select, arr, line, offset, banoff, copy_buffer, cursor):
 
 def copy(select, arr, line, offset, banoff, cursor):
     if select:
-        start = max(sum(select[0]), 0)
+        start = max(sum(select[0])-1, 0)
         copy_buffer = arr[start:sum(select[1])]
         if start> 0: copy_buffer = copy_buffer[1:]
     else:
         pos = line+offset-banoff
         text = arr[pos]
-        if cursor==len(text):
+        if cursor-1==len(text):
             if pos<len(arr)-1:
                 copy_buffer = arr[pos+1]
-        else: copy_buffer = text[cursor:]
+        else: copy_buffer = text[cursor-1:]
 
     if isinstance(copy_buffer,list) and len(copy_buffer)==1:
         copy_buffer = copy_buffer[0]
